92b612d129dfb4ff5f3a108565ca516ad34aaf08
ColacX Unknown <ColacX@hotmail.com>
2012-03-20 21:23:25 +0100

    removed some comments, some commented code and an unnecessary call to window resize

--- SuperSoup/Client/GameClient.cpp
+++ SuperSoup/Client/GameClient.cpp
@@ -205,8 +205,6 @@ public:
 void GameClient::run(){
 	Ground g;
 	bool b = g[b2Vec2(1.0f,2.0f)];
-	//B2_NOT_USED(argc);
-	//B2_NOT_USED(argv);
 
 	// Define the gravity vector.
 	b2Vec2 gravity(0.0f, -10.0f);
@@ -239,95 +237,12 @@ void GameClient::run(){
 	md.mass = 40.0f;
 	player->SetMassData(&md);
 	
-	/*
-	b2BodyDef groundBodyDef;
-	groundBodyDef.position.Set(0.0f, -1.0f);
-
-	// Call the body factory which allocates memory for the ground body
-	// from a pool and creates the ground box shape (also from a pool).
-	// The body is also added to the world.
-	b2Body* groundBody = world.CreateBody(&groundBodyDef);
-
-	// Define the ground box shape.
-	b2PolygonShape groundBox;
-
-	// The extents are the half-widths of the box.
-	groundBox.SetAsBox(100.0f, 1.0f);
-
-	// Add the ground fixture to the ground body.
-	groundBody->CreateFixture(&groundBox, 0.0f);
-
-	// Define the dynamic body. We set its position and call the body factory.
-	b2BodyDef bodyDef;
-	bodyDef.type = b2_dynamicBody;
-	bodyDef.position.Set(0.0f, 8.0f);
-	bodyDef.fixedRotation = false;
-	bodyDef.angle = 1.0f;
-	b2Body* body = world.CreateBody(&bodyDef);
-
-	// Define another box shape for our dynamic body.
-	b2PolygonShape dynamicBox;
-	dynamicBox.SetAsBox(1.0f, 1.0f);
-
-	// Define the dynamic body fixture.
-	b2FixtureDef fixtureDef;
-	fixtureDef.shape = &dynamicBox;
-
-	//bouncy ness
-	fixtureDef.restitution = 0.8f;
-
-	// Set the box density to be non-zero, so it will be dynamic.
-	fixtureDef.density = 1.0f;
-
-	// Override the default friction.
-	fixtureDef.friction = 0.8f;
-
-	// Add the shape to the body.
-	body->CreateFixture(&fixtureDef);
-	*/
 	// Prepare for simulation. Typically we use a time step of 1/60 of a
 	// second (60Hz) and 10 iterations. This provides a high quality simulation
 	// in most game scenarios.
 	float32 timeStep = 1.0f / 60.0f;
 	int32 velocityIterations = 1;//6;
 	int32 positionIterations = 1;//2;
-	/*
-	//----------------player------------------------------
-
-	//shape
-	b2PolygonShape playerShape;
-	playerShape.SetAsBox(1.0f, 1.0f);
-
-	//body def
-	b2BodyDef playerBodyDef;
-	playerBodyDef.type = b2_dynamicBody;
-	playerBodyDef.position.Set(3.0f, 8.0f);
-	playerBodyDef.fixedRotation = false;
-	playerBodyDef.angle = 1.0f;
-
-	//body
-	b2Body* playerBody = world.CreateBody(&playerBodyDef);
-
-	//fixture def
-	b2FixtureDef playerFixture;
-	playerFixture.shape = &playerShape;
-	playerFixture.restitution = 0.8f;
-	playerFixture.density = 1.0f;
-	playerFixture.friction = 0.8f;
-
-	//fixture
-	playerBody->CreateFixture(&playerFixture);
-
-	player = playerBody;
-	*/
-
-	// When the world destructor is called, all bodies and joints are freed. This can
-	// create orphaned pointers, so be careful about your world management.
-
-    //important thread required initiation
-    //some functions and methods must be called within this thread or they will be ignored
-    //because this is the render context onwning thread
-    //BOOL b = SetThreadPriority( GetCurrentThread(), THREAD_PRIORITY_HIGHEST); //sets thread priority
 
     //start window
     Window w0( false, "SuperSoup" );
@@ -348,7 +263,6 @@ void GameClient::run(){
     
 	if(!rwglMakeCurrent)
         throw "GameClient: wglMakeCurrent failed";
-	windowResized(window0->getWidth(),window0->getHeight());
 
     setVerticalSync(true);
 	windowResized(window0->getWidth(), window0->getHeight() );
@@ -364,11 +278,9 @@ void GameClient::run(){
 
 		//reset drawing buffer
         glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+		
 		//player->ApplyForceToCenter(b2Vec2(0.0f, 60 * 40.0f * timeStep));
 		//draw body stuff
-		// This is our little game loop.
-		// Instruct the world to perform a single step of simulation.
-		// It is generally best to keep the time step and iterations fixed.
 		world.Step(timeStep, velocityIterations, positionIterations);
 
 		//------------ Camera trixing ------------		
