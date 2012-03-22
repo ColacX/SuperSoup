eb406af98cc8ee381d7c6eb115c327f19370bc4c
ColacX Unknown <ColacX@hotmail.com>
2012-03-22 19:55:41 +0100

    added: memory leak check on the server

--- SuperSoup/Server/mainServer.cpp
+++ SuperSoup/Server/mainServer.cpp
@@ -1,9 +1,15 @@
 #pragma comment(lib, "Ws2_32.lib" ) //windows sockets v2 32 bit tcp-network library
 
+#include <crtdbg.h>
+
 #include "GameServer.hpp"
 
 int main(int argc, char** argv)
 {
+	//_CrtSetBreakAlloc(150);
+    //_RPT0(_CRT_WARN, "hello world\n");
+	printf("main: start\n");
+
 	try
 	{
 		GameServer gs;
@@ -14,6 +20,15 @@ int main(int argc, char** argv)
 		printf("%s\n", ex);
 		getchar();
 	}
+
+	printf("main: end\n");
+
+    //printing memory leaks takes a lot of time
+    //also deallocationg memory in a proper manner also takes alot of time
+	if( _CrtDumpMemoryLeaks() ){
+        printf("main: you have memory leaks!\n");
+        getchar();
+    }
 
 	return 1337;
 }
