31bec36c2b31b81708fa50138e713aa8bb3412ae
Boll <ZebbZorg@hotmail.com>
2012-03-29 12:44:44 +0200

    The terrain physics will soon work

--- SuperSoup/Client/GameClient.cpp
+++ SuperSoup/Client/GameClient.cpp
@@ -98,12 +98,12 @@ void GameClient::windowResized(int width, int height)
 	glMatrixMode(GL_PROJECTION);
 	glPushMatrix();
 	glLoadIdentity();
-	gluOrtho2D(0, gameWidth/10.0f,-gameHeight/10.0f, 0);
+	gluOrtho2D(0, gameWidth/20.0f,-gameHeight/20.0f, 0);
 	glMatrixMode(GL_MODELVIEW);
 	glPushMatrix();
 	glLoadIdentity();
 	
-	glTranslatef(+gameWidth/20.0f, -gameHeight/20.0f, 0);
+	glTranslatef(+gameWidth/40.0f, -gameHeight/40.0f, 0);
 }
 
 void GameClient::windowUnfocused(){
@@ -130,7 +130,7 @@ struct Object{
 	b2Color color;
 	void createBox( b2World& world,
 					b2Vec2 pos = b2Vec2_zero,
-					b2Vec2 size = b2Vec2(1.0f,1.0f),
+					b2Vec2 size = b2Vec2(0.5f,0.5f),
 					float32 angle = 0.0f,
 					b2BodyType objectType = b2_dynamicBody,
 					b2Color color = b2Color(0.5f+(0.5f*rand())/RAND_MAX,0.5f+(0.5f*rand())/RAND_MAX,0.5f+(0.5f*rand())/RAND_MAX) ){
@@ -154,9 +154,9 @@ struct Object{
 		//fixture def
 		b2FixtureDef playerFixture;
 		playerFixture.shape = &playerShape;
-		playerFixture.restitution = 0.8f;
+		playerFixture.restitution = 0.8f;	// air resistance / fluid resistance
 		playerFixture.density = 1.0f;
-		playerFixture.friction = 0.8f;
+		playerFixture.friction = 5.0f;
 
 		//fixture
 		body->CreateFixture(&playerFixture);
@@ -192,19 +192,121 @@ std::vector< Object > objects;
 
 class Ground{
 public:
-	bool isBlock(const b2Vec2& p) const{
-		if(p.y<0)
-			return true;
-		return false;
-	}
-	void draw(const b2Vec2& cameraPosition) const{
+	int sx;
+	int sy;
+
+	int oldPx;
+	int oldPy;
+
+	Object* bodies; // [sy][sx]
+	size_t count;
+
+	Ground(b2World& world){
+		sx = 64;
+		sy = 64;
 		
+		count = 0;
+		bodies = new Object[sx*sy];
+		for(int y=0; y<sy; ++y)
+			for(int x=0; x<sx; ++x)
+				bodies[x+y*sy].createBox(world,b2Vec2(x,y),b2Vec2(0.5f,0.5f),0.0f,b2_staticBody);
 	}
+	bool isBlock(int x, int y) const{
+		if(y>=0)
+			return false;
+
+		if(x%3==0 || x%5==0 || y%3==0 || y%5==0)
+			return false;
+
+
+		return true;
+	}
+	void drawCube(float x, float y) const{
+		b2Vec2 vertices[4];
+		float extend = 0.5f;
+		vertices[0] = b2Vec2(x - extend, y - extend);
+		vertices[1] = b2Vec2(x + extend, y - extend);
+		vertices[2] = b2Vec2(x + extend, y + extend);
+		vertices[3] = b2Vec2(x - extend, y + extend);
+
+		b2Color color(
+			unsigned int(47*x+13*y)%47 / 46.0f,
+			unsigned int(73*x+91*y)%37 / 36.0f,
+			unsigned int(51*x+11*y)%97 / 96.0f
+		);
+
+		DrawPolygon( vertices, 4, color );
+	}
+
+	void test(){
+
+	}
+
+	void calc(float cx, float cy){
+		b2Filter collisionDisabled;
+		b2Filter collisionEnabled;
+		collisionDisabled.maskBits =  0;
+		collisionEnabled.maskBits  = ~0;
+
+		count = 0;
+		int e = 1;
+		for(int y=-sy/2; y<=sy/2+1*e; y+=e){
+			for(int x=-sx/2; x<=sx/2+1*e; x+=e){
+				float bx = cx + x;
+				float by = cy + y;
+				int bxi = e*floor(bx/e);
+				int byi = e*floor(by/e);
+			
+
+				if( isBlock(bxi,byi) ){
+					//bodies[bxi+byi*sy].body->GetFixtureList()[0].SetFilterData(collisionEnabled);
+					//drawCube(bxi,byi);
+					bodies[count].body->SetTransform(b2Vec2(bxi,byi),0.0f);
+					++count;
+				}
+				//else
+				//	bodies[bxi+byi*sy].body->GetFixtureList()[0].SetFilterData(collisionDisabled);
+			}
+		}
+		for(int i=0; i<count; ++i)
+			bodies[i].body->GetFixtureList()[0].SetFilterData(collisionEnabled);
+		for(int i=count; i<sx*sy; ++i)
+			bodies[i].body->GetFixtureList()[0].SetFilterData(collisionDisabled);
+	}
+
+	void draw(){
+		for(int i=0; i<count; ++i)
+			drawCube(bodies[i].body->GetPosition().x,bodies[i].body->GetPosition().y);
+			//bodies[i].DebugDrawBox();
+	}
+
+	// CamereraPostionX, CameraPositionY
+	//void draw(float cx, float cy) const{
+	//	int e = 2.0f;
+	//	for(int y=-sy/2; y<=sy/2+1*e; y+=e){
+	//		for(int x=-sx/2; x<=sx/2+1*e; x+=e){
+	//			float bx = cx + x;
+	//			float by = cy + y;
+	//			int bxi = e*floor(bx/e);
+	//			int byi = e*floor(by/e);
+	//			
+	//			//b2Filter collisionDisabled;
+	//			//b2Filter collisionEnabled;
+	//			//collisionDisabled.maskBits =  0;
+	//			//collisionEnabled.maskBits  = ~0;
+
+	//			if( isBlock(bxi,byi) ){
+	//				//bodies[bxi+byi*sy].body->GetFixtureList()[0].SetFilterData(collisionEnabled);
+	//				drawCube(bxi,byi);
+	//			}
+	//			//else
+	//			//	bodies[bxi+byi*sy].body->GetFixtureList()[0].SetFilterData(collisionDisabled);
+	//		}
+	//	}
+	//}
 };
 
 void GameClient::run(){
-	Ground g;
-	bool b = g.isBlock(b2Vec2(1.0f,2.0f));
 	//B2_NOT_USED(argc);
 	//B2_NOT_USED(argv);
 
@@ -220,13 +322,20 @@ void GameClient::run(){
 	ground.createBox(world,b2Vec2(0,-1),b2Vec2(100,1),0,b2_staticBody);
 	objects.push_back(ground);
 	
-	for(float y=10.0f; y<=60.0f; y+=2.0f){
-		for(float x=-70.0f; x<=70.0f; x+=2.0f){
+	size_t antal = 0;
+	for(float y=10.0f; y<=30.0f; y+=1.0f){
+		for(float x=-20.0f; x<=20.0f; x+=1.0f){
 			Object other;
 			other.createBox(world,b2Vec2(x,y));
 			objects.push_back(other);
+			++antal;
 		}
 	}
+	printf( "\nantal boxar som skapas = %u\n\n", antal );
+
+	
+	Ground g(world);
+	//bool b = g.isBlock(b2Vec2(1.0f,2.0f));
 	
 	Object playerObj;
 	playerObj.createBox(world,b2Vec2(0,30));
@@ -235,7 +344,7 @@ void GameClient::run(){
 	player = playerObj.body;
 	b2MassData md;
 	md.center = b2Vec2_zero;
-	md.I = 2.6666667f;
+	md.I = 2.6666667f;	// 8/3 is the default value.
 	md.mass = 40.0f;
 	player->SetMassData(&md);
 	
@@ -289,8 +398,8 @@ void GameClient::run(){
 	// second (60Hz) and 10 iterations. This provides a high quality simulation
 	// in most game scenarios.
 	float32 timeStep = 1.0f / 60.0f;
-	int32 velocityIterations = 1;//6;
-	int32 positionIterations = 1;//2;
+	int32 velocityIterations = 6;
+	int32 positionIterations = 2;
 	/*
 	//----------------player------------------------------
 
@@ -369,11 +478,12 @@ void GameClient::run(){
 		// This is our little game loop.
 		// Instruct the world to perform a single step of simulation.
 		// It is generally best to keep the time step and iterations fixed.
+		g.calc(player->GetPosition().x, player->GetPosition().y);
 		world.Step(timeStep, velocityIterations, positionIterations);
 
 		//------------ Camera trixing ------------		
 		glLoadIdentity();
-		glTranslatef(+gameWidth/20.0f, -gameHeight/20.0f, 0);
+		glTranslatef(+gameWidth/40.0f, -gameHeight/40.0f, 0);
 		glTranslatef(-player->GetPosition().x,-player->GetPosition().y,0.0f);
 		printf("x:%f\ty:%f\n", player->GetPosition().x, player->GetPosition().y);
 
@@ -386,8 +496,12 @@ void GameClient::run(){
 		//DebugDrawBox(*playerBody, playerShape);
 		//o.DebugDrawBox();
 
+		g.draw();
+
 		for(size_t o=0; o<objects.size(); ++o)
 			objects[o].DebugDrawBox();
+
+		//g.draw(player->GetPosition().x,player->GetPosition().y);
 
         window0->swapBuffers();
 
